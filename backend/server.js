93d79165e26e13415f335b9ec8b1ce39111dbd28
Manoj <manojyadav23s04@gmail.com>
2025-12-11 16:19:53 +0530

    update CORS configuration for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://badminton-court-booking-system-ten.vercel.app/"
+        ],
+        credentials: true
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
